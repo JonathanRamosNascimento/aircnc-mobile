e1eef22c1dd51d4d103e9b53889ff79b8cd9b4a2
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-10-06 00:24:00 -0300

    iniciando a criação da list de spots

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View ,SafeAreaView, Text, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    // Estou usando o View e não o SafeAreaView por que meu emulador é android e não adaptou com o Safe
+    return (
+        <View style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 70
+    }
+})
